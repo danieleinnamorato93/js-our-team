2f6a1b6bdb2dc3cb5caa4e0e0ed299d98bd50743
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-08-28 16:33:45 +0200

    #Milestone #2 complete

--- script.js
+++ script.js
@@ -1,23 +1,11 @@
 console.log('ok')
 /*Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
 Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
-
-MILESTONE 1:
-Stampare su console le informazioni di nome, ruolo e la stringa della foto
-MILESTONE 2:
-Stampare le stesse informazioni su DOM sottoforma di stringhe
-
-
- # DATI
-Wayne Barnett |	Founder & CEO        |	wayne-barnett-founder-ceo.jpg
-Angela Caroll |	Chief Editor         |	angela-caroll-chief-editor.jpg
-Walter Gordon |	Office Manager       |	walter-gordon-office-manager.jpg
-Angela Lopez  |	Social Media Manager |	angela-lopez-social-media-manager.jpg
-Scott Estrada |	Developer            |	scott-estrada-developer.jpg
-Barbara Ramos |	Graphic Designer     |	barbara-ramos-graphic-designer.jpg 
 */ 
 
 //MILESTONE 0: Creo l’array di oggetti con le informazioni fornite
+
+const infosElement = document.getElementById('infos');
 
 const people = [
     {
@@ -52,5 +40,32 @@ const people = [
     }
 
 ];
+/*MILESTONE 1:
+Stampare su console le informazioni di nome, ruolo e la stringa della foto*/
 
 console.table(people);
+
+//MILESTONE 2:Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+//utilizzo un ciclo for per iterare nell'array
+for(let i=0 ; i < people.length ; i++) {
+    const person= people[i];
+
+
+
+// Costruisco una stringa HTML per ciascun membro del team
+
+const personInfo = `<p>
+<strong>Nome:</strong> ${person.name} <br>
+<strong>Ruolo:</strong> ${person.role} <br>
+<strong>Link foto:</strong> ${person.photo} <br>
+</p>` ;
+
+//aggiungo la stringa in pagina
+
+infosElement.innerHTML += personInfo;
+
+}
+
+
+
